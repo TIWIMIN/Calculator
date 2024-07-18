0f7837181ceece933b2dde6c16d67170dc79f02d
TIWIMIN <tiwimin@gmail.com>
2024-07-17 19:27:48 -0700

    Begin input handling logic with event delegation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let firstNum;
 let secondNum; 
 let operator; 
 
+let displayValue; 
 
 function add (a, b) {
     return a + b;
@@ -37,3 +38,19 @@ function operate(firstNum, secondNum, operator) {
     }
 }
 
+let key0 = document.querySelector("#key0");
+let key1 = document.querySelector("#key1");
+let key2 = document.querySelector("#key2");
+let key3 = document.querySelector("#key3");
+let key4 = document.querySelector("#key4");
+let key5 = document.querySelector("#key5");
+let key6 = document.querySelector("#key6");
+let key7 = document.querySelector("#key7");
+let key8 = document.querySelector("#key8");
+let key9 = document.querySelector("#key9");
+
+let digitPress = document.querySelector(".digit")
+digitPress.addEventListener("click", (e) => {
+    let digit = e.target.id; 
+    console.log(digit);
+})
